ba34583c55f85a660e9d930bf68c0510e1b26aaf
PadovaJ <114314608+PadovaJ@users.noreply.github.com>
2022-10-02 21:51:11 -0400

    having trouble with this

---
+++ week 1 quiz.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+var input = fs.readFileSync(process.stdin.fd, "utf-8").trim().split("\n");
+var num1 = parseInt(input[0]);
+var num2 = parseInt(input[1]);
+
+console.log(num2 -num1 * 16);
